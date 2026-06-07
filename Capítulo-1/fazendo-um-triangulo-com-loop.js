a626fa1e113b2c46cd36f2bba0b0c1ef2aa78c1e
6SixFrosty <galaxyslayer0009@gmail.com>
2026-06-07 00:09:28 -0300

    Exercício: Triangulo com loop, leve fix

--- Capítulo-1/fazendo-um-triangulo-com-loop.js
+++ Capítulo-1/fazendo-um-triangulo-com-loop.js
@@ -2,22 +2,22 @@
  * Exercício: Fazendo um Triângulo
  * Enunciado: Escreva um loop que faça sete chamadas a console.log
  * para exibir o seguinte triângulo:
-    #
-    ##
-    ###
-    ####
-    #####
-    ######
-    #######
+ *  #
+ *  ##
+ *  ###
+ *  ####
+ *  #####
+ *  ######
+ *  #######
  */
 
 // Declarando a variável que vai ser utilizada e o caractere que vai ser utilizado
 let char = "#";
- 
-// Inicializando o loop com uma variavel "a" para contar 7 execuções
+
+// Inicializando o loop com uma variável "a" para contar 7 execuções
 for (let a = 0; a < 7; a++) {
-    // imprimir no console o caractere
+    // Imprime no console o caractere
     console.log(char);
-    // a cada interação do loop e incrementado o caractere
-    char += "#"
+    // A cada iteração do loop é incrementado o caractere
+    char += "#";
 }

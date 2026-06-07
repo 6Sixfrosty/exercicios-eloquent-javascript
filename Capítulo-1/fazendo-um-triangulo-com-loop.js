20274dd799839280262708a21c29358bb4a041bd
6SixFrosty <galaxyslayer0009@gmail.com>
2026-06-06 23:43:20 -0300

    codigo comentado

--- Capítulo-1/fazendo-um-triangulo-com-loop.js
+++ Capítulo-1/fazendo-um-triangulo-com-loop.js
@@ -13,7 +13,11 @@
 
 // Declarando a variável que vai ser utilizada e o caractere que vai ser utilizado
 let char = "#";
+ 
+// Inicializando o loop com uma variavel "a" para contar 7 execuções
 for (let a = 0; a < 7; a++) {
+    // imprimir no console o caractere
     console.log(char);
+    // a cada interação do loop e incrementado o caractere
     char += "#"
 }
